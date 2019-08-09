493c7cc1c75add633f5787eb3af0ba250ea87a6b
Oscar Prado <oprado7b6@users.noreply.github.com>
2019-08-09 13:24:47 -0700

    Add files via upload

--- LaVendimia_Node/LaVendimiaStart.js
+++ LaVendimia_Node/LaVendimiaStart.js
@@ -1,31 +1,55 @@
 var http = require('http');
 var fs = require('fs');
-var MongoClient = require('mongodb').MongoClient;
-//var dbo = require('./database_connection');
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const DATABASE_NAME = "LaVendimia";
 const URL = "mongodb://localhost:27017/${DATABASE_NAME}";
+const client = new MongoClient(URL, { useNewUrlParser: true });
+
+client.connect(function(err, client) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server");
+	const db = client.db(DATABASE_NAME);
+	ventasActivas(db, function() {
+		client.close();
+	});
+});
 
 http.createServer(function (req, res) {
+	let rawdata = fs.readFileSync('ventasActivas.json');
+	let venta = JSON.parse(rawdata);
+	//console.log(venta);
 	fs.readFile('LaVendimia.html', function(err, data) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);0
+		res.write(data);
+		console.log(venta);
 		res.end();
 	});
-	/*
-	const client = new MongoClient.connect(URL, { useNewUrlParser: true });
-	var dbo = null;
-	try {
-		// Note this breaks.
-		// await client.connect({useNewUrlParser: true})
-		//await client.connect();
-		dbo = client.db(DATABASE_NAME);
-		dbo.collection("Articulos").find({}, { projection: { _id: 1, descripcion: 1, modelo: 1, cantidad: 1, precio: 1 } }).toArray(function(err, result) {
-		if (err) throw err;
-		console.log(result);
-		//db.close();
-	});	} catch (err) {
-		console.log(err.stack);
-	}
-	//var dbo = require('./database_connection');
-*/
 }).listen(8080);
+
+function ventasActivas(db, callback) {
+	db.collection('Ventas').aggregate([
+		{ $lookup:
+			{
+			from: 'VentasDetalle',
+			localField: 'folio',
+			foreignField: 'folio',
+			as: 'articulos'
+			}
+		}
+	]).toArray(function(err, res) {
+		if (err) throw err;
+		var jsonObj = JSON.parse(JSON.stringify(res));
+		var jsonContent = JSON.stringify(jsonObj);
+		//console.log(jsonObj);
+		//console.log(jsonContent);
+		fs.writeFile("ventasActivas.json", jsonContent, 'utf8', function (err) {
+			if (err) {
+				console.log("An error occured while writing JSON Object to File.");
+				return console.log(err);
+			}
+			console.log("JSON file has been saved.");
+		});
+	});
+};
