87bb232f8f1c2ddc115c50629e8dd944bcaefe96
Oscar Prado <oprado7b6@users.noreply.github.com>
2019-08-10 16:22:11 -0700

    Add files via upload

---
+++ LaVendimia_Node/start_lv.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE, { useMongoClient: true });
+mongoose.Promise = global.Promise;
+mongoose.connection
+	.on('connected', () => {
+		console.log('Mongoose connection open on ${process.env.DATABASE}');
+	})
+	.on('error', (err) => {
+		console.log('Connection error: ${err.message}');
+	});
+	
+require('./models/LaVendimia');
+const app = require('./app');
+const server = app.listen(4040, () => {
+	console.log('Express is running on port ${server.address().port}');
+});
+
+
