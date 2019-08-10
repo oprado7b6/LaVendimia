9780418851def012ce4d7f90fd8bcfe0a01ee2fd
Oscar Prado <oprado7b6@users.noreply.github.com>
2019-08-10 16:20:36 -0700

    Add files via upload

---
+++ LaVendimia_Node/schemas.js
@@ -0,0 +1,67 @@
+var schemas = {
+	acceso: {
+		id: null,
+		modulo: null,
+		activo: null,
+		fecha_alta: null,
+		fecha_baja: null
+	}
+	articulo: {
+		id: null,
+		descripcion: null,
+		modelo: null,
+		cantidad: null,
+		precio: null,
+		color: null
+	}
+	cliente: {
+		id: null,
+		nombre: null,
+		primer_apellido: null,
+		segundo_apellido: null,
+		rfc: null,
+		direccion: null,
+		telefono: null
+	}
+	plazo: {
+		id: null,
+		plazo: null,
+		periodo: null,
+		tasa: null
+	}
+	usuario: {
+		id: null,
+		clave_acceso: null,
+		nombre: null,
+		primer_apellido: null,
+		segundo_apellido: null,
+		tipo_usuario: null,
+		activo: null,
+		fecha_registro: null
+	}
+	venta: {
+		id: null,
+		folio: null,
+		cliente: null,
+		bonificacion: null,
+		enganche: null,
+		total: null,
+		tasa_financiamiento: null,
+		plazo_maximo: null,
+		fecha: null,
+		estatus: null,
+		vendedor: null
+	}
+	venta_articulos: {
+		id: null,
+		folio: null,
+		articulo: null,
+		cantidad: null,
+		precio: null,
+		importe: null,
+		promocion: null,
+		estatus: null
+	}
+}
+
+module.exports = schemas;
