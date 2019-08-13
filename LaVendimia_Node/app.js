aed29043dacaff869b539d637a3cca57b2f5e227
Oscar Prado <oprado7b6@users.noreply.github.com>
2019-08-13 13:51:35 -0700

    Add files via upload

--- LaVendimia_Node/app.js
+++ LaVendimia_Node/app.js
@@ -1,32 +1,40 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const db = process.env.DATABASE;
-const models = require('./models');
+const app = express();
 
-mongoose.connect(db, { useMongoClient: true });
+// define routes
+var indexRouter = require('./routes/index');
+var catalogRouter = require('./routes/catalog');
+
+var models = require('./models/index');
+
+var events = require('events');
+var util = require('util');
+var eventsEmitter = new events.EventEmitter();
+
+//// Database connection to mongoDB 
+mongoose.connect(db, { useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 mongoose.connection.on('connected', () => {
 		console.log('Mongoose connection open on ${process.env.DATABASE} '+ db );
 	})
 	.on('error', (err) => {
-		console.log('Connection error: ${err.message}');
+		console.log('Unable Connection error: ${err.message}', err);
 	});
-	
-//const routes = require('./routes/index');
-const app = express();
-const fs = require('fs');
+/// End database connection
 
-const modelsPath = path.resolve(__dirname, 'models')
-fs.readdirSync(modelsPath).forEach(file => {
-  require(modelsPath + '/' + file);
-})
+//////////////////////////////////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////////////////////////////
 // configure app
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
+//app.set('views', path.join('__dirname', 'views'));
+app.set('views', path.join(__dirname, '/views/'));
+app.set('view engine', 'ejs');
 //app.set('view engine', 'pug');
 
 // use middleware 
@@ -34,12 +42,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname,'bower_components')));
-app.use(express.static(path.join(__dirname,'public')));
 app.use(express.static('public'));
 
-// define routes
-var todosRoutes = require('./todos');
-app.use(todosRoutes);
-//app.use('./', routes);
+app.use('/', indexRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next){
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// development error handler will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+ 
+// production error handler no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
 
 module.exports = app;
